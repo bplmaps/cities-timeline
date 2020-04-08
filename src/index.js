c0db410d64af5069e24136be9aab31b7e6bffe36
Garrett Dash Nelson <garrettdashnelson@gmail.com>
2020-04-07 21:33:51 -0400

    collection filtering

--- src/index.js
+++ src/index.js
@@ -13,78 +13,87 @@ var zoomContainer = document.getElementById('zoom-container-outer');
 
 var zoomInitialized = false;
 
-var database = new Airtable({apiKey: 'key10KeIIPJpM8npJ'}).base('appFHShc770eIgtu9');
+var database = new Airtable({ apiKey: 'key10KeIIPJpM8npJ' }).base('appFHShc770eIgtu9');
 
-database('Timeline Objects').select({view: "Grid view", filterByFormula: "{Status} = 'Published'"}).firstPage().then(result => { initialize(result); });
+database('Timeline Objects')
+	.select({ view: 'Grid view', filterByFormula: "{Status} = 'Published'" })
+	.firstPage()
+	.then((result) => {
+		initialize(result);
+	});
 
 var layer;
 var map;
 
-
 function loadZoomable(imageInfoUrl) {
-  fetch(imageInfoUrl).then(function(response) {
-    response.json().then(function(imageInfo) {
-      var options = new IIIFInfo(imageInfo).getTileSourceOptions();
-      if (options === undefined || options.version === undefined) {
-        console.log('Data seems to be no valid IIIF image information.');
-        return;
-      }
-      options.zDirection = -1;
-      var iiifTileSource = new IIIF(options);
-      layer.setSource(iiifTileSource);
-      map.setView(new View({
-        resolutions: iiifTileSource.getTileGrid().getResolutions(),
-        extent: iiifTileSource.getTileGrid().getExtent(),
-        constrainOnlyCenter: true
-      }));
-      map.getView().fit(iiifTileSource.getTileGrid().getExtent());
-      console.log(imageInfoUrl);
-    }).catch(function() {
-      console.log('Could not read image info json');
-    });
-  }).catch(function() {
-    console.log('Could not read data from URL.');
-  });
+	fetch(imageInfoUrl)
+		.then(function(response) {
+			response
+				.json()
+				.then(function(imageInfo) {
+					var options = new IIIFInfo(imageInfo).getTileSourceOptions();
+					if (options === undefined || options.version === undefined) {
+						console.log('Data seems to be no valid IIIF image information.');
+						return;
+					}
+					options.zDirection = -1;
+					var iiifTileSource = new IIIF(options);
+					layer.setSource(iiifTileSource);
+					map.setView(
+						new View({
+							resolutions: iiifTileSource.getTileGrid().getResolutions(),
+							extent: iiifTileSource.getTileGrid().getExtent(),
+							constrainOnlyCenter: true
+						})
+					);
+					map.getView().fit(iiifTileSource.getTileGrid().getExtent());
+					console.log(imageInfoUrl);
+				})
+				.catch(function() {
+					console.log('Could not read image info json');
+				});
+		})
+		.catch(function() {
+			console.log('Could not read data from URL.');
+		});
 }
 
-// loadZoomable('https://iiif.digitalcommonwealth.org/iiif/2/commonwealth:ht2502616/info.json');
+document.addEventListener(
+	'click',
+	function(event) {
+		if (event.target.matches('.zoom-button')) {
+			zoomContainer.style.display = 'block';
+			if (!zoomInitialized) {
+				layer = new TileLayer();
+				map = new Map({
+					layers: [ layer ],
+					target: 'zoom-viewer'
+				});
+				zoomInitialized = true;
+			} else {
+				layer.setSource(null);
+			}
+			loadZoomable(event.target.getAttribute('data-iiif') + '/info.json');
+			map.render();
+		}
 
-
-document.addEventListener('click', function (event) {
-
-	if (event.target.matches('.zoom-button')) {
-        zoomContainer.style.display = 'block';
-        if(!zoomInitialized) {
-            layer = new TileLayer();
-        map = new Map({
-          layers: [layer],
-          target: 'zoom-viewer'
-        });
-        zoomInitialized = true;
-        } else {
-            layer.setSource(null);
-        }
-        loadZoomable(event.target.getAttribute('data-iiif') + '/info.json');
-        map.render();
-	}
-
-	if (event.target.matches('#zoom-closer')) {
-		zoomContainer.style.display = 'none';
-	}
-
-}, false);
-
-
+		if (event.target.matches('#zoom-closer')) {
+			zoomContainer.style.display = 'none';
+		}
+	},
+	false
+);
 
 function initialize(slides) {
+	var timelineSlides = [];
+	slides.forEach((s) => {
+		timelineSlides.push(parseSlide(s));
+	});
 
-    var timelineSlides = [];
-    slides.forEach(s => { timelineSlides.push( parseSlide(s) ); } );
-
-    const title = {
-        text: {
-            headline: "Mapping a World of Cities",
-            text: `<p>How, where, and why do cities develop? Maps are one of our best tools for answering these questions. Cities are founded and grow in particular locations, driven by geographical features like fresh water or deep harbors as well as historical events like royal charters or speculators’ claims. From the earliest walled towns, individuals and families living in close proximity to one another have shaped urban form by building infrastructure and common space according to their economic, cultural, religious, and military needs. With the rise of nations, empires, and trading networks, cities became linked together, both with other cities and with their own hinterlands.</p>
+	const title = {
+		text: {
+			headline: 'Mapping a World of Cities',
+			text: `<p>How, where, and why do cities develop? Maps are one of our best tools for answering these questions. Cities are founded and grow in particular locations, driven by geographical features like fresh water or deep harbors as well as historical events like royal charters or speculators’ claims. From the earliest walled towns, individuals and families living in close proximity to one another have shaped urban form by building infrastructure and common space according to their economic, cultural, religious, and military needs. With the rise of nations, empires, and trading networks, cities became linked together, both with other cities and with their own hinterlands.</p>
 
             <p>Looking at maps helps us to understand the changing geography of urban life. Maps didn’t just serve as snapshots of how cities looked at one moment in time; in the form of plans, maps were also used to build, speculate, and fight over urban form. Historical maps reflect cities’ ethnic and economic transformations, systems of domination and oppression, sites of monumentality and squalor. They capture good times and bad, expansion, decay, and destruction. City dwellers take great pride in their cities, as part of a shared sense of place that is part of a historical trajectory. Maps tell the stories of a city’s past, present—and perhaps its future.</p>
             
@@ -102,51 +111,85 @@ function initialize(slides) {
             <li>Verygood Map Collection</li>
             <li>Histor Ical Library at the University of the Humanities</li>
             </ul>`
-        }
-    }
+		}
+	};
 
-    var timeline = new TL.Timeline('app',{title: title, events: timelineSlides});
+	var timeline = new TL.Timeline('app', { title: title, events: timelineSlides });
 
+	document.getElementById('collection-filter').addEventListener('change', function() {
+		var c = this.value;
 
-    // timeline.config.events.forEach(function(d) { if(d.group != "North America") { timeline.removeId(d.unique_id); console.log("removed" + d.unique_id); } })
-
+		if (c === 'all') {
+      timelineSlides.forEach(function(d){
+        if(!d.active){ timeline.add(d); d.active = true; }
+       });
+		} else {
+			timelineSlides.forEach(function(d) {
+				if (d.collection === c && !d.active) {
+					timeline.add(d);
+					d.active = true;
+				}
+			});
+			// we have to do this loop twice so we don't potentially remove all the slides before we add any new ones back in; timeline doesn't like having no slides
+			timelineSlides.forEach(function(d) {
+				if (d.collection != c && d.active) {
+					timeline.removeId(d.unique_id);
+					d.active = false;
+				}
+			});
+		}
+	});
 }
 
 function parseSlide(s) {
+	var explodedDate = s.get('Position Date').split('-');
+	var jsonDate = { year: +explodedDate[0], month: +explodedDate[1], day: +explodedDate[2] };
 
-    var explodedDate = s.get('Position Date').split('-')
-    var jsonDate = {year: +explodedDate[0], month: +explodedDate[1], day: +explodedDate[2] }
+	var thumbnail = s.get('IIIF Endpoint') ? `${s.get('IIIF Endpoint')}/square/500,/0/default.jpg` : '';
 
-    var thumbnail = s.get('IIIF Endpoint') ? `${s.get('IIIF Endpoint')}/square/500,/0/default.jpg` : '';
+	if (s.get('IIIF Endpoint')) {
+		var caption = `<button class="zoom-button" data-iiif="${s.get('IIIF Endpoint')}">Zoom in</button>`;
+	} else {
+		var caption = '';
+	}
 
-    if(s.get('IIIF Endpoint')) {
-        var caption = `<button class="zoom-button" data-iiif="${s.get('IIIF Endpoint')}">Zoom in</button>`;
-    } else {
-        var caption = '';
-    }
-
-
-    return {
-        unique_id: s.get('ID'),
-        text: {
-            headline: fallback(s,'Caption Title'),
-        text: `<p>${fallback(s,'Caption')}</p>
-        <p class="citation"><span class="object-title">${s.get('Object Title')}</span><br><span class="object-creator">${s.get('Object Creator')}</span><br><span class="object-date">${s.get('Display Date')}</span><br><span class="object-dimensions">${s.get('Dimensions')}</span><br><span class="contributing-institution">${s.get('Contributing Institution')}</span><br><a class="repository-url" href="${s.get('Repository URL')}" target="_blank">Link to Collections Record</a></p>`
-        },
-        start_date: jsonDate ,
-        display_date: s.get('Display Date'),
-        group: s.get('Region'),
-        media: {
-            url: s.get('Image URL'),
-            link: s.get('Repository URL'),
-            thumbnail: thumbnail,
-            caption: caption
-        }
-    }
-
+	return {
+		unique_id: s.get('ID').toString(),
+		collection: s.get('Contributing Institution'),
+		active: true,
+		text: {
+			headline: fallback(s, 'Caption Title'),
+			text: `<p>${fallback(s, 'Caption')}</p>
+        <p class="citation"><span class="object-title">${s.get(
+			'Object Title'
+		)}</span><br><span class="object-creator">${s.get(
+				'Object Creator'
+			)}</span><br><span class="object-date">${s.get(
+				'Display Date'
+			)}</span><br><span class="object-dimensions">${s.get(
+				'Dimensions'
+			)}</span><br><span class="contributing-institution">${s.get(
+				'Contributing Institution'
+			)}</span><br><a class="repository-url" href="${s.get(
+				'Repository URL'
+			)}" target="_blank">Link to Collections Record</a></p>`
+		},
+		start_date: jsonDate,
+		display_date: s.get('Display Date'),
+		// group: s.get('Region'),
+		media: {
+			url: s.get('Image URL'),
+			link: s.get('Repository URL'),
+			thumbnail: thumbnail,
+			caption: caption
+		}
+	};
 }
 
-
-function fallback(s,field) {
-    if( s.get(field) ) { return s.get(field); } else { return `${field} not entered`; }
+function fallback(s, field) {
+	if (s.get(field)) {
+		return s.get(field);
+	} else {
+		return `${field} not entered`;
+	}
 }
